6d13c0ac8737785523f87a6b29843e63f728f217
Gustav98 <gustavo.oliveira141998@hotmail.com>
2020-07-21 19:43:31 -0300

    Adicionar e carregar novos textos na tabela

--- src/components/Tabela.vue
+++ src/components/Tabela.vue
@@ -1,5 +1,34 @@
 <template v-if="dadosCarregados">
   <div>
+    <template>
+      <v-form
+        ref="form"
+        @submit.prevent="salvarNovoTexto"
+      >
+          <v-text-field
+            v-model="texto.title"
+            label="Título"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="texto.body"
+            label="Descrição"
+            required
+          ></v-text-field>
+          
+          <v-btn
+            color="success"
+            class="mr-4"
+            @click="salvarNovoTexto"
+          >
+            Salvar
+          </v-btn>
+
+      </v-form>
+    </template>
+    <v-spacer></v-spacer>
+
     <v-data-table
       :headers="headers"
       :items="items"
@@ -18,7 +47,7 @@
       <v-pagination v-model="page" :length="pageCount"></v-pagination>
       <v-text-field
         :value="itemsPerPage"
-        label="Items per page"
+        label="Itens por página"
         type="number"
         min="-1"
         max="15"
@@ -42,14 +71,29 @@
       headers: [
         { text: 'Título', value: 'title' },
         { text: 'Descrição', value: 'body' },
-      ]
+      ],
+      texto: {
+        title: '',
+        body: ''
+      }
     }),
     async mounted() {
-      await Textos.listar().then( response => {
-        this.items = response;
-        console.log(this.items);
+      await Textos.listar().then( resposta => {
+        this.items = resposta;
         this.dadosCarregados = true;
       })
+    },
+    methods: {
+      salvarNovoTexto(){
+        Textos.adicionar(this.texto).then( resposta => {
+          let textoSalvo = resposta.data;
+          localStorage.setItem('textoSalvo', textoSalvo);
+          this.items = [
+            textoSalvo,
+            ...this.items
+          ]
+        })
+      }
     }
   }
 </script>
--- src/services/textos.js
+++ src/services/textos.js
@@ -8,4 +8,8 @@ export default {
         return consultaDados;
     },
 
+    adicionar: (texto) => {
+        return http.post('/posts', texto)
+    }
+
 }
